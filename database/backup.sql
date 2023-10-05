9d233dcd3ed12347cb29c9dbec114a9d096cb6ce
JohanLuque <luquejohan194@gmail.com>
2023-10-04 20:21:33 -0500

    backup antes de la entrega

--- database/backup.sql
+++ database/backup.sql
@@ -33,8 +33,8 @@ CREATE TABLE `animales` (
 /*Data for the table `animales` */
 
 insert  into `animales`(`idAnimal`,`nombreAnimal`,`create_at`,`update_at`,`estado`) values 
-(1,'Perro','2023-09-22 09:41:17',NULL,'1'),
-(2,'Gato','2023-09-22 09:41:17',NULL,'1');
+(1,'Perro','2023-10-04 20:20:33',NULL,'1'),
+(2,'Gato','2023-10-04 20:20:33',NULL,'1');
 
 /*Table structure for table `clientes` */
 
@@ -51,18 +51,15 @@ CREATE TABLE `clientes` (
   `estado` char(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`idCliente`),
   UNIQUE KEY `uk_dni_cli` (`dni`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `clientes` */
 
 insert  into `clientes`(`idCliente`,`apellidos`,`nombres`,`dni`,`claveAcceso`,`create_at`,`update_at`,`estado`) values 
-(1,'Luque Ramos','Johan Alberto','71789712','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-09-22 09:41:19',NULL,'1'),
-(2,'Quispe Acevedo','Piero','12345678','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-09-22 09:41:19',NULL,'1'),
-(3,'Polo Andrade','Andy Smith','12345677','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-09-22 09:41:19',NULL,'1'),
-(4,'Corzo Carbajal','Aldo Alfredo','12345666','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-09-22 09:41:19',NULL,'1'),
-(5,'Lopez','Lopez','21790932','$2y$10$qSe.Xtel1ibj6qPJZgcd9.BbS/pCqhg7Hea8zBhmWodrHe4v3pvUm','2023-09-22 09:46:39',NULL,'1'),
-(6,'fernando','luis','36963698','12345','2023-09-22 09:48:40',NULL,'1'),
-(7,'Lopez','Juana','21790939','$2y$10$tNz5uXmBseQdGQTR1yRPLuR75GStKOAb8i4IXe.oIUigR.lTrx5NS','2023-09-22 09:49:07',NULL,'1');
+(1,'Luque Ramos','Johan Alberto','71789712','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-10-04 20:20:33',NULL,'1'),
+(2,'Quispe Acevedo','Piero','12345678','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-10-04 20:20:33',NULL,'1'),
+(3,'Polo Andrade','Andy Smith','12345677','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-10-04 20:20:33',NULL,'1'),
+(4,'Corzo Carbajal','Aldo Alfredo','12345666','$2y$10$Cnti6nMp5glSTbDRJy9bAOHWeYXI5uRSvEcKnKFMA8qU2D6jJFNCW','2023-10-04 20:20:33',NULL,'1');
 
 /*Table structure for table `mascotas` */
 
@@ -86,11 +83,11 @@ CREATE TABLE `mascotas` (
 /*Data for the table `mascotas` */
 
 insert  into `mascotas`(`idMascota`,`nombre`,`fotografia`,`color`,`genero`,`idCliente`,`idRaza`) values 
-(1,'Carrizo','carrizo.jpg','Negro y Rubio','H',1,4),
-(2,'Blanca','blanca.jpg','Blanco','H',1,6),
-(3,'Conejo','conejo.jpg','Rubio','M',2,8),
-(4,'sofi','sofi.jpg','Rubio','H',2,3),
-(5,'Garfield','carfield.jpg','Rubio','M',2,12);
+(1,'Carrizo','perro1.jpg','Negro y Rubio','H',1,4),
+(2,'Blanca','perro2.jpg','Blanco','H',1,6),
+(3,'Conejo','gato2.jpg','Rubio','M',2,8),
+(4,'sofi','oerro1.jpg','Rubio','H',2,3),
+(5,'Garfield','gato3.jpg','Rubio','M',2,12);
 
 /*Table structure for table `razas` */
 
@@ -112,18 +109,18 @@ CREATE TABLE `razas` (
 /*Data for the table `razas` */
 
 insert  into `razas`(`idRaza`,`nombreRaza`,`idAnimal`,`create_at`,`update_at`,`estado`) values 
-(1,'Schnauser',1,'2023-09-22 09:41:17',NULL,'1'),
-(2,'Dalmata',1,'2023-09-22 09:41:17',NULL,'1'),
-(3,'American Bully',1,'2023-09-22 09:41:17',NULL,'1'),
-(4,'Pastor aleman',1,'2023-09-22 09:41:17',NULL,'1'),
-(5,'Pit bull',1,'2023-09-22 09:41:17',NULL,'1'),
-(6,'Mestizo',1,'2023-09-22 09:41:17',NULL,'1'),
-(7,'Chinchilla',2,'2023-09-22 09:41:17',NULL,'1'),
-(8,'Devon Rex',2,'2023-09-22 09:41:17',NULL,'1'),
-(9,'Korat',2,'2023-09-22 09:41:17',NULL,'1'),
-(10,'Oriental de pelo largo',2,'2023-09-22 09:41:17',NULL,'1'),
-(11,'Ragdoll',2,'2023-09-22 09:41:17',NULL,'1'),
-(12,'Ocigato',2,'2023-09-22 09:41:17',NULL,'1');
+(1,'Schnauser',1,'2023-10-04 20:20:33',NULL,'1'),
+(2,'Dalmata',1,'2023-10-04 20:20:33',NULL,'1'),
+(3,'American Bully',1,'2023-10-04 20:20:33',NULL,'1'),
+(4,'Pastor aleman',1,'2023-10-04 20:20:33',NULL,'1'),
+(5,'Pit bull',1,'2023-10-04 20:20:33',NULL,'1'),
+(6,'Mestizo',1,'2023-10-04 20:20:33',NULL,'1'),
+(7,'Chinchilla',2,'2023-10-04 20:20:33',NULL,'1'),
+(8,'Devon Rex',2,'2023-10-04 20:20:33',NULL,'1'),
+(9,'Korat',2,'2023-10-04 20:20:33',NULL,'1'),
+(10,'Oriental de pelo largo',2,'2023-10-04 20:20:33',NULL,'1'),
+(11,'Ragdoll',2,'2023-10-04 20:20:33',NULL,'1'),
+(12,'Ocigato',2,'2023-10-04 20:20:33',NULL,'1');
 
 /* Procedure structure for procedure `spu_buscar_clientes` */
 
@@ -141,6 +138,52 @@ BEGIN
 	INNER JOIN Razas ON Mascotas.idRaza = Razas.idRaza
 	INNER JOIN Animales ON Razas.idAnimal = Animales.idAnimal
 	WHERE clientes.dni = _dni;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `spu_listar_Animal` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `spu_listar_Animal` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `spu_listar_Animal`()
+BEGIN 
+	SELECT idAnimal, nombreAnimal 
+	FROM animales;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `spu_listar_mascotas` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `spu_listar_mascotas` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `spu_listar_mascotas`(
+	IN _idcliente 	INT
+)
+BEGIN
+	SELECT mascotas.idmascota,clientes.nombres, clientes.apellidos, mascotas.nombre, mascotas.color, mascotas.genero
+	FROM mascotas
+	INNER JOIN clientes ON clientes.idcliente = mascotas.idcliente
+	INNER JOIN razas ON razas.idraza = mascotas.idraza
+	WHERE mascotas.idcliente = _idcliente;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `spu_listar_razas` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `spu_listar_razas` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `spu_listar_razas`( IN _id INT)
+BEGIN
+	SELECT idraza, 
+	nombreRaza 
+	FROM razas
+	WHERE idanimal = _id;
 END */$$
 DELIMITER ;
 
@@ -167,10 +210,10 @@ DELIMITER ;
 DELIMITER $$
 
 /*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `spu_registrar_clientes`(
-IN _apellidos		VARCHAR(30),
+IN _apellidos			VARCHAR(30),
 IN _nombres			VARCHAR(30),
 IN _dni				CHAR(8),
-IN _claveAcceso	VARCHAR(200)
+IN _claveAcceso			VARCHAR(200)
 )
 BEGIN
 	INSERT INTO Clientes(apellidos, nombres, dni, claveAcceso) VALUES
@@ -207,15 +250,14 @@ DELIMITER ;
 DELIMITER $$
 
 /*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `spu_search_mascotas`(
-IN _idMascota	INT 
+	IN _idmascota 	INT
 )
 BEGIN
-	SELECT clientes. Animales.nombreAnimal, razas.nombreRaza, Mascotas.fotografia, Mascotas.nombre, Mascotas.color, Mascotas.genero 
-	FROM Mascotas
-	INNER JOIN Clientes ON Mascotas.idCliente = Clientes.idCliente
-	INNER JOIN Razas ON Mascotas.idRaza = Razas.idRaza
-	INNER JOIN Animales ON Razas.idAnimal = Animales.idAnimal
-	WHERE Mascotas.idMascota =  _idMascota;
+	SELECT mascotas.idmascota,mascotas.nombre,razas.nombreRaza,animales.nombreAnimal, mascotas.fotografia ,mascotas.color, mascotas.genero
+	FROM mascotas
+	INNER JOIN razas ON razas.idraza = mascotas.idraza
+	INNER JOIN animales ON animales.idanimal = razas.idanimal
+	WHERE mascotas.idmascota = _idmascota;
 END */$$
 DELIMITER ;
 
